b7a6a91df0583b1170f1ff0112b3f8360f476750
eloco <22910374+Eloco@users.noreply.github.com>
2023-07-24 16:42:34 +0800

    modified:   app.py 	modified:   docker-compose.yml 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template, jsonify
 from PIL import Image
 import pytesseract
 import io
+import requests
 
 app = Flask(__name__)
 executor = ThreadPoolExecutor(2)
@@ -15,30 +16,46 @@ def process_image(image_data, lang, config):
     return text
 
 
+def process_image_from_url(image_url, lang, config):
+    response = requests.get(image_url)
+    image_data = response.content
+    return process_image(image_data, lang, config)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Read the uploaded image data
-        image_file = request.files['image']
-        image_data = image_file.read()
-
         # Get the language parameter from the request
         lang = request.form.get('lang', 'eng')  # Default language is English ('eng')
 
         # Get the config parameter from the request
-        config = request.form.get('config', '')  # Default config is empty string
+        config = request.form.get('config', '')  # Default config is an empty string
 
-        # Process the image using OCR
-        future = executor.submit(process_image, image_data, lang, config)
-
-        # Get the OCR result
-        result = future.result()
-
-        # Return the result as JSON response
-        return jsonify({'result': result})
+        # Check if the image is uploaded or provided as a URL
+        if 'image' in request.files:
+            # Read the uploaded image data
+            image_file = request.files['image']
+            image_data = image_file.read()
+            # Process the image using OCR
+            future = executor.submit(process_image, image_data, lang, config)
+            # Get the OCR result
+            result = future.result()
+            # Return the result as JSON response
+            return jsonify({'result': result})
+        elif 'image_url' in request.form:
+            # Get the image URL from the request
+            image_url = request.form['image_url']
+            # Process the image from the URL using OCR
+            future = executor.submit(process_image_from_url, image_url, lang, config)
+            result = future.result()
+            # Return the result as JSON response
+            return jsonify({'result': result})
+        else:
+            # Return an error response if no image or image URL is provided
+            return jsonify({'error': 'No image or image URL provided.'}), 400
 
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=7777)
